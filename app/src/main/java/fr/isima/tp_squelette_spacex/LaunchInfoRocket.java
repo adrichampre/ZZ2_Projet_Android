9bdc911d0cfa8789f814a1482ea36288108fca63
Adrien Champredonde <adrien.champredonde@etu.udamail.fr>
2020-02-14 14:30:48 +0100

    Correction LaunchInfoRocket

--- app/src/main/java/fr/isima/tp_squelette_spacex/LaunchInfoRocket.java
+++
@@ -1,5 +0,0 @@
-package fr.isima.tp_squelette_spacex;
-
-public class LaunchInfoRocket {
-    public String rocket_name;
-}
---
+++ app/src/main/java/fr/isima/tp_squelette_spacex/adapter/LaunchInfoRocket.java
@@ -0,0 +1,7 @@
+package fr.isima.tp_squelette_spacex.adapter;
+
+import java.io.Serializable;
+
+public class LaunchInfoRocket implements Serializable {
+    public String rocket_name;
+}
